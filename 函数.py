de2759c66060dcbec1717de28027a8f9688f9625
qiuyelengyusw <qiuyelengyu@qq.com>
2023-05-02 11:22:22 +0800

    添加注释

--- 函数.py
+++ 函数.py
@@ -1,6 +1,6 @@
-def greet(name):
-    print("Hi" + " " + name)
-    return
+def greet(name): #声明函数
+    print("Hi" + " " + name)     #编写函数的功能
+    return   #编写函数结束语句
 
 
-greet("mike")
+greet("mike")   #调用函数
