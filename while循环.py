f4d8fd5064ba20547edcd37a781654dc5604604f
qiuyelengyusw <qiuyelengyu@qq.com>
2023-05-02 10:46:30 +0800

    修改暂停时间为1秒

--- while循环.py
+++ while循环.py
@@ -2,7 +2,7 @@
 Author: qiuyelengyusw qiuyelengyu@qq.com
 Date: 2023-05-02 09:54:12
 LastEditors: qiuyelengyusw qiuyelengyu@qq.com
-LastEditTime: 2023-05-02 10:28:05
+LastEditTime: 2023-05-02 10:34:33
 FilePath: \MyPythonProject\while循环.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -16,7 +16,7 @@ while player_lift > 0 and enemy_lift > 0:    #当玩家血量与敌人血量都�
     enemy_lift = enemy_lift - player_attack   #敌人血量等于敌人原来的血量减去玩家攻击力
     print("the enemy launch an attack,the player's blood:" + str(player_lift))
     print("the player launch an attack,the enemy's blood:" + str(enemy_lift))
-    time.sleep(2) #暂停2秒
+    time.sleep(1) #暂停2秒
 if player_lift > 0 >= enemy_lift:  #如果玩家的血量大于零并且敌人的血量小于等于零的时候，判定玩家获胜
     print("The player Win!")
 else:     #反之则判断敌人获胜
