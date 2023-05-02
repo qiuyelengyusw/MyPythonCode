3c4e0492823f18a27603b8395cb4560a40b18b19
qiuyelengyusw <qiuyelengyu@qq.com>
2023-05-02 11:43:32 +0800

    函数的基本练习

--- 函数.py
+++ 函数.py
@@ -1,6 +1,18 @@
-def greet(name): #声明函数
-    print("Hi" + " " + name)     #编写函数的功能
-    return   #编写函数结束语句
+def greet(name):  # 声明函数
+    print("你好" + " " + name)  # 编写函数的功能
+    return  # 编写函数结束语句
 
 
-greet("mike")   #调用函数
+def age_1(age):
+    if age < 15:
+        return "你还是儿童，还可以犯傻！"
+    else:
+        return "你已经是个成年人了，心里有点儿数吧！"
+
+
+print("请问你的名字叫什么？")
+strName = input()
+print("请问你的年龄是多少？")
+intAge = int(input())
+greet(strName)
+print(age_1(intAge))
