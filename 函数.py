2e12d5b46466e2248547fdf125aee3d202a01492
qiuyelengyusw <qiuyelengyu@qq.com>
2023-05-04 15:49:07 +0800

    添加注释
    Signed-off-by: qiuyelengyusw <qiuyelengyu@qq.com>

--- 函数.py
+++ 函数.py
@@ -5,14 +5,14 @@ def greet(name):  # 声明函数
 
 def age_1(age):
     if age < 15:
-        return "你还是儿童，还可以犯傻！"
+        return "你还是儿童，还可以犯傻！" #返回字符串
     else:
         return "你已经是个成年人了，心里有点儿数吧！"
 
 
 print("请问你的名字叫什么？")
-strName = input()
+strName = input() #将用户输入的内容赋值给strName.
 print("请问你的年龄是多少？")
-intAge = int(input())
-greet(strName)
+intAge = int(input()) #类型转换
+greet(strName) #调用函数
 print(age_1(intAge))
