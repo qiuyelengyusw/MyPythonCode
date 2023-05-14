8de6fa2a82f660a044475cc5a3e341d417b9fe7e
qiuyelengyusw <qiuyelengyu@qq.com>
2023-05-14 19:49:54 +0800

    增加窗体代码

--- windows.py
+++ windows.py
@@ -1,5 +1,17 @@
 import tkinter
+import tkinter.messagebox
+
 nW = tkinter.Tk()
 nW.title("Python窗体")
 nW.geometry("600x600")
+
+
+def HelloWorld():
+    tkinter.messagebox.showinfo("hello", tl.get())
+
+
+bl = tkinter.Button(nW, text="按钮", command=HelloWorld)
+bl.grid(row=2, column=8)
+tl=tkinter.Entry(nW)
+tl.grid(row=0,column=0)
 nW.mainloop()
