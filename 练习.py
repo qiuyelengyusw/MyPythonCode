7fbc5892a683923810353f4788e76ad6eb4a94d0
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-08 15:20:53 +0800

    随机练习

---
+++ 练习.py
@@ -0,0 +1,9 @@
+print("What's your name?")
+name = input()
+print("Your name is %s"%name)
+print("How old are you?")
+age = int(input())
+print("your age is"+ str(age))
+print("your age is %s"%age)
+list = [1,2,3,4,"a","b","c",'a','b','c']
+print(list)
