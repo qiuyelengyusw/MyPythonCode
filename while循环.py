a731720a89e7e7dd1f9ed6cb3c0a2427a1a84831
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-08 20:07:47 +0800

    08

--- while循环.py
+++ while循环.py
@@ -2,7 +2,7 @@
 Author: qiuyelengyusw qiuyelengyu@qq.com
 Date: 2023-05-02 09:54:12
 LastEditors: qiuyelengyusw qiuyelengyu@qq.com
-LastEditTime: 2023-05-02 10:34:33
+LastEditTime: 2023-06-08 20:05:23
 FilePath: \MyPythonProject\while循环.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
