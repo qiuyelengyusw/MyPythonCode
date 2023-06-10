2f199a5d3b3eaf6191e121d5c809d05071ad438e
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-10 14:28:49 +0800

    while循环的练习

---
+++ while循环.py
@@ -0,0 +1,30 @@
+love = "yes"
+while love == "yes":
+    print("Do you love me today?")
+    love = str(input())
+i = 1
+sumi = 0
+while i <= 100:
+    i += 1
+    sumi += i
+    print(i)
+print(sumi)
+
+while True:
+    print('Can I exit?')
+    answer1 = str(input())
+    if answer1 == "yes":
+        print('you can exit this')
+        # 跳出循环体
+        break
+    else:
+        print('shit!')
+
+# 使用while 循环以及 continue 语句实现输出奇数
+c = 0
+while c < 100:
+    c += 1
+    if c % 2 == 0:
+        continue    # 当 C 可以被2 整除的时候，跳过下面的print(c),重新回到条件判断语句，如果满足条件，继续执行循环体的语句
+    print(c)
+# 总结，break 语句作用是跳出整个循环体，而continue语句的作用是跳出本次循环，回到循环开始的判断条件语句，如果满足循环条件，则再次运行循环体内的代码
