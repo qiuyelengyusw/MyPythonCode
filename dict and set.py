49d3734e6f50c0a7ea8d18ac5e4013d32c7eab93
qiuyelengyu <qiuyelengyu@qq.com>
2023-06-12 15:54:59 +0800

    01

--- dict and set.py
+++ dict and set.py
@@ -2,7 +2,7 @@ g = {'mike': 95, 'john': 85, 'sw': 100}
 print(g['mike'])
 print(g['sw'])
 
-# 把数据放入dict的方法，除了初始化时指定外，还可以通过key放入：
+# 把数据放入dict的方法，除了初始化时指定外，还可以通过key放入
 g['jike'] = 99
 print(g['jike'])
 print(g)
