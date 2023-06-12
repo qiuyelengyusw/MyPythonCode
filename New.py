33f37ce716d3182467228a373ec9d09959015b55
qiuyelengyu <qiuyelengyu@qq.com>
2023-06-12 15:34:18 +0800

    格式代码

--- New.py
+++ New.py
@@ -1,21 +1,19 @@
-#while 语句
+# while 语句
 a, b = 0, 1
-while b < 10:    
+while b < 10:
     a, b = b, a + b
     print(b)
 
-
-list = [1,2,3,4,5,6,7,8,9,10]
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 it = iter(list)
 for x in it:
-    print(x ,end=" ")
+    print(x, end=" ")
 
+# 使用netx()函数
 
+import sys  # 引入sys模块
 
-#使用netx()函数
-
-import sys #引入sys模块
-list1 = [1,2,3,4,5,6,7,8,9,10]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 it1 = iter(list1)
 while True:
     try:
