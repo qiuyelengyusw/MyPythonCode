1496ef3324c7ef5d61944012c6dd3389f197509a
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-13 16:32:18 +0800

    打印列表中指定位置的值，另一种方法

--- 列表.py
+++ 列表.py
@@ -9,8 +9,11 @@ for stx in List2:
 # 用while语句实现上面的代码
 print('下面的代码使用while语句来实现上面的for语句的功能')
 k = 0
-while k < len(List2):   #len函数用来获取列表或者字符串的长度
+while k < len(List2):  # len函数用来获取列表或者字符串的长度
     print(List2[k])
     k += 1
 print('下面语句实现打印列表中指定位置的值')
 print(List2[5])
+print(List2[len(List2) - 2])
+# 下面的语句与上面的语句实现同样的功能
+print(List2[-5])
