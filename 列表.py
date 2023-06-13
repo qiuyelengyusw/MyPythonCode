57ac0b898a131e9083119888b11a129c8f475881
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-13 15:46:31 +0800

    列表的初步接触

---
+++ 列表.py
@@ -0,0 +1,14 @@
+List1 = [1, 2, 3, 4, 5]
+List2 = [1, 2, 3, 4, 5, 'list']
+print(List1)
+print(List2)
+print(len(List2))
+for stx in List2:
+    print(stx)
+
+# 用while语句实现上面的代码
+print('下面的代码使用while语句来实现上面的for语句的功能')
+k = 0
+while k < len(List2):
+    print(List2[k])
+    k += 1
