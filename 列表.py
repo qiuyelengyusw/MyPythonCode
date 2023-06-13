aec44ee7ae51e96cc58d631e5ba3b94478ac00b3
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-13 16:24:00 +0800

    len函数用来获取列表或者字符串的长度

--- 列表.py
+++ 列表.py
@@ -9,6 +9,6 @@ for stx in List2:
 # 用while语句实现上面的代码
 print('下面的代码使用while语句来实现上面的for语句的功能')
 k = 0
-while k < len(List2):
+while k < len(List2):   #len函数用来获取列表或者字符串的长度
     print(List2[k])
     k += 1
