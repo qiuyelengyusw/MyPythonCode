e53ac958d8b9cb17af3973e7cae7b497678f998a
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-14 14:08:58 +0800

    用列表推导式来实现将列表中的元素值都乘以2

--- .vs/MyPythonProject/列表推导式.py
+++ .vs/MyPythonProject/列表推导式.py
@@ -6,3 +6,8 @@ for i in range(len(lista)):
     print(i)
     lista[i] = lista[i] * 2
 print(lista)
+
+# 下面的代码用列表推导式来实现将列表中的元素值都乘以2
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista = [i * 2 for i in lista]
+print(lista)
