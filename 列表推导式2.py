1226dc31ed4e9b0ca85c2d8c4a275baeae8efaef
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-19 15:59:51 +0800

    复习使用列表推导式来筛选列表中含有某个特定字符

--- 列表推导式2.py
+++ 列表推导式2.py
@@ -11,3 +11,10 @@ A = [[0] * 3 for i in range(3)]
 print(A)
 A[0][0] = 10
 print(A)
+
+A = ['Find', 'Fuck', 'English', 'Chinese', 'Good', 'Shit', 'Fish']
+for each in A:
+    if each[0] == 'F':
+        print(each)
+B = [b for b in A if b[0] == 'F']
+print(B)
