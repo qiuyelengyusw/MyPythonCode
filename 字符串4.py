335497fa9610e99839a5d0f8864a613b272d9b6b
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-20 14:33:59 +0800

    rstrip() 去掉字符串右侧的空白

--- 字符串4.py
+++ 字符串4.py
@@ -1,6 +1,11 @@
 # 截取字符串
 
 # lstrip() 去掉字符串左侧的空白
-x = '     I love you'
+x = '     左侧不要留空白'
 print(x)
 print(x.lstrip())
+print()
+# rstrip() 去掉字符串右侧的空白
+x = '右侧不要留空白       '
+print(x)
+print(x.rstrip())
