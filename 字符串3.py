27a2b63a2fbd475645770e682c04029142822605
qiuyelengyusw <qiuyelengyu@qq.com>
2023-06-20 13:18:56 +0800

    endswith() 用法与startswith()相反，判断字符串中指定的字符是否出现在字符串的末尾

--- 字符串3.py
+++ 字符串3.py
@@ -1,3 +1,8 @@
 # startswith() 判断字符串中的指定字符是否出现在字符串的起始位置，输出 True 或者 False
 x = '我正在学习python'
 print(x.startswith('我'))
+print(x.startswith('正在'))
+
+# endswith() 用法与startswith()相反，判断字符串中指定的字符是否出现在字符串的末尾
+print(x.endswith('python'))
+print(x.endswith('习'))
