b2c4e93efcee0d18705a993bd7df0d8074dcb07a
Administrator <qiuyelengyu@qq.com>
2025-03-08 15:40:14 +0800

    for循环以及range()的应用

--- for.py
+++ for.py
@@ -8,12 +8,25 @@ for a1 in range(101):
     print(suma)
 
 # 输出100以内所有的素数
-for n1 in range(2, 100):
-    for n2 in range(2, n1):
-        if n1 % n2 == 0:
+for n1 in range(2, 100): # 2到100的整数
+    for n2 in range(2, n1): # 2到n1的整数
+        if n1 % n2 == 0: #  n1除以n2的余数为0
             break
     else:
         print(n1, '是一个素数')
 
 for i in range(1, 6):
     print(i)
+print("----------------------------------------")
+import math
+
+# 输出100以内所有的素数
+for n1 in range(2, 100):
+    is_prime = True
+    # 只需要检查到n1的平方根
+    for n2 in range(2, int(math.sqrt(n1)) + 1):
+        if n1 % n2 == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(n1, '是一个素数')
---
+++ for循环.py
@@ -0,0 +1,48 @@
+#打印输出字符串中的每一个字符
+for each in "Qiuyelengyu":
+    print(each)
+print()
+#用while循环语句实现同样的功能
+str = "Qiuyelengyu"
+length = len(str)
+index = 0
+while index < length:
+    print(str[index])
+    index += 1
+#for循环和while循环的区别
+
+#用for循环打印输出1到100之间的奇数
+for i in range(1,101):
+    if i % 2 == 1:
+        print(i)
+#用for循环实现1到1000之间所有数的和
+sum = 0
+for i in range(1,1001):
+    sum += i
+print(sum)
+#for循环求1到10之间所有奇数的和
+sum = 0
+for i in range(1,11):
+    if i % 2 == 1:
+        sum += i
+print(sum)
+# for循环求1到10之间所有偶数的和
+sum = 0
+for i in range(1,11):
+     if i % 2 == 0:
+         sum += i
+print(sum)
+
+# for循环实现1到10之间所有偶数的乘积
+sum = 1
+for i in range(1,11):
+    if i % 2 == 0:
+        sum *= i
+print(sum)
+
+#for循环实现1到10之间所有奇数的乘积
+sum = 1 #乘积的初始值为1
+for i in range(1,11):  #range(1,11)表示1到10之间的所有数
+    if i % 2 == 1:     #判断i是否为奇数
+        sum *= i       #如果i为奇数，则将i乘以sum的值，然后将结果赋值给sum
+print(sum)            # 输出sum的值
