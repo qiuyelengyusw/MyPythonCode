3988cc386ccf852cf7996f84efbdabf3eb5d28a2
Administrator <qiuyelengyu@qq.com>
2025-03-09 13:25:37 +0800

    转义字符的应用

---
+++ 转义字符.py
@@ -0,0 +1,20 @@
+# 制表符
+# 在程序中，使用\t可以在控制台输出一个制表符，协助在输出文本时对齐
+print('W\t*')
+print("WW\t*")
+print("WWW\t*")
+
+# /n 换行
+print("这是第一行\n这是第二行 \n这是第三行")
+
+
+# \\
+print("C:\\windows\\system32\\cmd.exe")
+
+#
+print('他说:"我是一个程序员"')
+print("It's a good day" )
+# 转义字符 \
+print('It\'s a good day')
+print("他说：\"我是一个程序员\"")
+print("他说：\"我是一个程序员\"")
